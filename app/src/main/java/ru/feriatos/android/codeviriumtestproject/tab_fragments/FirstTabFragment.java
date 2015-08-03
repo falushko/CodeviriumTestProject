56b2e4d9052ed77ead47e2d0e064b093a4a7130b
Владимир <vov8278@yandex.ru>
2015-08-03 23:40:03 +0300

    database insert and list view added

--- app/src/main/java/ru/feriatos/android/codeviriumtestproject/tab_fragments/FirstTabFragment.java
+++ app/src/main/java/ru/feriatos/android/codeviriumtestproject/tab_fragments/FirstTabFragment.java
@@ -1,19 +1,36 @@
 package ru.feriatos.android.codeviriumtestproject.tab_fragments;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import ru.feriatos.android.codeviriumtestproject.R;
 
 public class FirstTabFragment extends Fragment {
 
+    ListView mListView;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_first_tab, container, false);
+
+        mListView = (ListView)v.findViewById(R.id.listView);
+
+        final String[] catnames = new String[] {
+                "Рыжик", "gkgfhjg", "Мурзик", "Мурка", "Васька",
+                "Томасина", "Кристина", "Пушок", "Дымка", "Кузя",
+                "Китти", "Масяня", "Симба"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),	android.R.layout.simple_list_item_1, catnames);
+
+        mListView.setAdapter(adapter);
+
         return v;
     }
 }
--- app/src/main/java/ru/feriatos/android/codeviriumtestproject/tab_fragments/SecondTabFragment.java
+++ app/src/main/java/ru/feriatos/android/codeviriumtestproject/tab_fragments/SecondTabFragment.java
@@ -1,22 +1,78 @@
 package ru.feriatos.android.codeviriumtestproject.tab_fragments;
 
-
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import ru.feriatos.android.codeviriumtestproject.R;
+import ru.feriatos.android.codeviriumtestproject.SqlDataManager;
 
 /**
  * Created by hp1 on 21-01-2015.
  */
 public class SecondTabFragment extends Fragment {
+
+    EditText mFirstName;
+    EditText mLastName;
+    EditText mPhone;
+    EditText mEmail;
+    Button mSendButton;
+    String[] mUserData = new String[4];
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_second_tab, container, false);
+
+        mFirstName = (EditText) v.findViewById(R.id.firstName);
+        mLastName = (EditText) v.findViewById(R.id.lastName);
+        mPhone = (EditText) v.findViewById(R.id.phone);
+        mEmail = (EditText) v.findViewById(R.id.eMail);
+        mSendButton = (Button) v.findViewById(R.id.send);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                mUserData[0] = String.valueOf(mFirstName.getText());
+                mUserData[1] = String.valueOf(mLastName.getText());
+                mUserData[2] = String.valueOf(mPhone.getText());
+                mUserData[3] = String.valueOf(mEmail.getText());
+
+                for (String str : mUserData){
+                    if(str.isEmpty()){
+                        Toast toast = Toast.makeText(getActivity(), "Fill all fields", Toast.LENGTH_SHORT);
+                        toast.show();
+                        return;
+                    }
+                }
+
+                SqlDataManager sqlManager = new SqlDataManager(getActivity());
+                // Gets the data repository in write mode
+                SQLiteDatabase db = sqlManager.getWritableDatabase();
+
+                // Create a new map of values, where column names are the keys
+                ContentValues values = new ContentValues();
+                values.put(SqlDataManager.NAME, mUserData[0]);
+                values.put(SqlDataManager.LAST_NAME, mUserData[1]);
+                values.put(SqlDataManager.PHONE, Integer.parseInt(mUserData[2]));
+                values.put(SqlDataManager.EMAIL, mUserData[3]);
+
+                db.insert(SqlDataManager.TABLE_NAME, null, values);
+
+                Toast toast = Toast.makeText(getActivity(), "Data have been sent", Toast.LENGTH_SHORT);
+                toast.show();
+
+            }
+        });
+
         return v;
     }
 }
